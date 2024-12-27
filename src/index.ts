efa42d3c94c2c26874a9f765734e81b000d2def1
nadhifikbarw <nadhifikbarw@gmail.com>
2024-12-27 11:52:47 +0700

    fix: export more types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 export { Airtable } from "./airtable";
+export type { AirtableBase } from "./base";
+export type { AirtableTable } from "./table";
+export type { AirtableQuery } from "./query";
+export type { AirtableRecord } from "./record";
+export type { AirtableComment } from "./comment";
+export type { AirtableError } from "./error";
+export type { AirtableCommentQuery } from "./comment-query";
+export type { RetryDelayOption } from "./retry";
+
 export type * from "./types";
